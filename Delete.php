83383844a8367d7ecb531f3ed101c127d3cea231
63070162 <73683141+63070162@users.noreply.github.com>
2020-11-17 21:31:30 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect("labitf.mysql.database.azure.com", "it63070162@labitf", "0994424004Art", "lapitf", 3306) 
+$conn=mysqli_connect("labitf.mysql.database.azure.com", "it63070162@labitf", "0994424004Art", "labitf", 3306) 
 or die("connect error");
 
 ?>
