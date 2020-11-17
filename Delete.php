695d613b42e5bfa0aab55d2b985f98d79304d769
63070162 <73683141+63070162@users.noreply.github.com>
2020-11-17 21:44:06 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,5 +1,7 @@
 <?php
 $conn=mysqli_connect("labitf.mysql.database.azure.com", "it63070162@labitf", "0994424004Art", "labitf", 3306) 
 or die("connect error");
-
+$ID=$_GET["ID"];
+$delete_sql="Delete from guestbook where ID=".$ID."";
+$Delete_query=mysqli_query($conn, $delete_sql);
 ?>
