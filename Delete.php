a9d2bdf485329e7841a9654eb9a170329a4e9f48
63070162 <73683141+63070162@users.noreply.github.com>
2020-11-21 22:38:50 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,13 +1,40 @@
 <?php
-$conn=mysqli_connect("labitf.mysql.database.azure.com", "it63070162@labitf", "0994424004Art", "labitf", 3306) 
-or die("connect error");
-$ID=$_GET["ID"];
-$delete_sql="Delete from guestbook where ID=".$ID."";
-$Delete_query=mysqli_query($conn, $delete_sql);
-if ($Delete_query){
-    echo "Delete successful";
-}
-else{
-    echo "failed to delete";
-}
+	$conn = mysqli_connect('beta.mysql.database.azure.com', 'it63070113@beta', 'EMLcnk22', 'ITFLab');
+	$sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'';
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete Zone</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+	<style type="text/css">
+		.card {
+			border-top: solid 5px #37cf23;
+		}
+	</style>
+</head>
+<body class="bg-light py-5">
+	<div class="container text-dark">
+		<div class="row">
+			<div class="col-12 col-lg-8 offset-lg-2">
+				<div class="card shadow">
+					<div class="card-body">
+						<h2 align="center">
+						<?php
+							if(mysqli_query($conn, $sql)) {
+								echo "DELETE COMPLETED";
+							}
+							else {
+								echo "FAILED TO DELETE";
+							}
+						?>
+						</h2>
+						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-sm btn-success">BACK</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
